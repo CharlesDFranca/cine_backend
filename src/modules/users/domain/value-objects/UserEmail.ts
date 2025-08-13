257c6b3d192f4ee3d46a7268cba4ab6fddc7b850
CharlesDFranca <josecharlestill@gmail.com>
2025-08-13 19:45:40 -0300

    Creation of value-objects 'Username', 'Useremail' and 'UserPassword'

---
+++ src/modules/users/domain/value-objects/UserEmail.ts
@@ -0,0 +1,32 @@
+type UserEmailProps = { value: string };
+
+export class UserEmail {
+  private constructor(private readonly props: UserEmailProps) {}
+
+  static create(props: UserEmailProps): UserEmail {
+    const email = props.value.trim().toLocaleLowerCase();
+
+    if (!email) {
+      throw new Error("O email não pode ser vazio");
+    }
+
+    if (email.includes("..")) {
+      throw new Error(
+        `O email não pode conter conter pontos consecutivos: ${email}`,
+      );
+    }
+
+    const EMAIL_REGEX =
+      /^(?!.*\.\.)(?!\.)([a-zA-Z0-9._%+-]*[a-zA-Z0-9_%+-])@[a-zA-Z0-9-]+(\.[a-zA-Z0-9-]+)*\.[a-zA-Z]{2,}$/;
+
+    if (!EMAIL_REGEX.test(email)) {
+      throw new Error(`O email está em um formato inválido: ${email}`);
+    }
+
+    return new UserEmail({ value: email });
+  }
+
+  get value() {
+    return this.props.value;
+  }
+}
---
+++ src/modules/users/domain/value-objects/UserPassword.ts
@@ -0,0 +1,57 @@
+type UserPasswordProps = {
+  value: string;
+  isHashed?: boolean;
+};
+
+export class UserPassword {
+  private constructor(private readonly props: UserPasswordProps) {}
+
+  static create(props: UserPasswordProps): UserPassword {
+    const password = props.value.trim();
+
+    if (!password) {
+      throw new Error("A senha não pode ser vazia");
+    }
+
+    const MIN_LENGTH = 8;
+    const MAX_LENGTH = 128;
+
+    if (password.length < MIN_LENGTH) {
+      throw new Error(`A senha não pode ser tão curta: [MIN: ${MIN_LENGTH}]`);
+    }
+
+    if (password.length > MAX_LENGTH) {
+      throw new Error(`A senha não pode ser tão longa: [MAX: ${MAX_LENGTH}]`);
+    }
+
+    const hasLowercase = /[a-z]/;
+
+    if (!hasLowercase.test(password)) {
+      throw new Error("A senha deve ter pelo menos uma letra minúscula");
+    }
+
+    const hasUppercase = /[A-Z]/;
+
+    if (!hasUppercase.test(password)) {
+      throw new Error("A senha deve ter pelo menos uma letra maiúscula");
+    }
+
+    const hasNumber = /\d/;
+
+    if (!hasNumber.test(password)) {
+      throw new Error("A senha deve ter pelo menos um número");
+    }
+
+    const hasSpecialChar = /[^a-zA-Z0-9]/;
+
+    if (!hasSpecialChar.test(password)) {
+      throw new Error("A senha deve ter pelo menos um caractere especial");
+    }
+
+    return new UserPassword({ value: password });
+  }
+
+  get value(): string {
+    return this.props.value;
+  }
+}
