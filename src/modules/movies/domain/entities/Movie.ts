df49e85cbf5a80e5279423c7953d2539098c2abc
AtlasM30 <moreirarthur@gmail.com>
2025-08-13 20:55:34 -0300

    Uptade movies props using value objects

--- src/modules/movies/domain/entities/Movie.ts
+++ src/modules/movies/domain/entities/Movie.ts
@@ -1,11 +1,19 @@
+import type { MovieClassification } from "../value-objects/MovieClassification.js";
+import type { MovieDuration } from "../value-objects/MovieDuration.js";
+import type { MovieGenre } from "../value-objects/MovieGenre.js";
+import type { MovieObservation } from "../value-objects/MovieObservation.js";
+import type { MoviePlatform } from "../value-objects/MoviePlatform.js";
+import type { MovieTitle } from "../value-objects/MovieTitle.js";
+
 type MovieProps = {
-  title: string;
-  genre: string;
-  classification: number;
-  platform: string;
-  showtime: number;
-  duration: number;
-  observation: string;
+  title: MovieTitle;
+  genre: MovieGenre;
+  userId: string;
+  classification: MovieClassification;
+  platform: MoviePlatform;
+  showtime: Date;
+  duration: MovieDuration;
+  observation?: MovieObservation;
   watched: boolean;
   rating: number;
   createdAt: Date;
