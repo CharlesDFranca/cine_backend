e79c40f72bd30373097e61bfe009b1ed440b365f
CharlesDFranca <josecharlestill@gmail.com>
2025-08-13 20:58:14 -0300

    update user properties to use value objects

--- src/modules/users/domain/entities/User.ts
+++ src/modules/users/domain/entities/User.ts
@@ -1,7 +1,11 @@
+import type { UserEmail } from "../value-objects/UserEmail.js";
+import type { UserName } from "../value-objects/UserName.js";
+import type { UserPassword } from "../value-objects/UserPassword.js";
+
 type UserProps = {
-  name: string;
-  email: string;
-  password: string;
+  name: UserName;
+  email: UserEmail;
+  password: UserPassword;
   createdAt: Date;
   updatedAt: Date;
 };
@@ -14,5 +18,17 @@ export class User {
 
   static create(props: UserProps, _id: string) {
     return new User(_id, props);
+  }
+
+  get name(): UserName {
+    return this.props.name;
+  }
+
+  get email(): UserEmail {
+    return this.props.email;
+  }
+
+  get password(): UserPassword {
+    return this.props.password;
   }
 }
