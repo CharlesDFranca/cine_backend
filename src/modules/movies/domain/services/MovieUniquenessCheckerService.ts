138af2e44c7063bdaf9e9c332a8926f913a7f122
AtlasM30 <moreirarthur@gmail.com>
2025-08-16 20:39:10 -0300

    Uptade of service MovieUniquenessCheckerService

--- src/modules/movies/domain/services/MovieUniquenessCheckerService.ts
+++ src/modules/movies/domain/services/MovieUniquenessCheckerService.ts
@@ -4,14 +4,13 @@ import { IMovieUniquenessCheckerService } from "./contracts/IMovieUniquenessChec
 export class MovieUniquenessCheckerService
   implements IMovieUniquenessCheckerService
 {
-  check(currentMovie: Movie, movies: Movie[]): void {
-    movies.forEach((movie) => {
-      if (
-        currentMovie.title.value === movie.title.value &&
-        currentMovie.showtime.getTime() === movie.showtime.getTime()
-      ) {
-        throw new Error("O filme já foi registrado na data marcada");
-      }
-    });
+  async check(
+    currentMovie: Movie,
+    movieExists: (movie: Movie) => Promise<boolean>,
+  ): Promise<void> {
+    const movie = await movieExists(currentMovie);
+    if (movie) {
+      throw new Error("O filme já foi registrado na data marcada");
+    }
   }
 }
