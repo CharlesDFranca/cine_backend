384e4a3c0a011ce57106ac646e26a7cc89c8a5a3
CharlesDFranca <josecharlestill@gmail.com>
2025-08-18 10:58:32 -0300

    Creating UserControllers and implementing the route to create users

--- src/main.ts
+++ src/main.ts
@@ -6,10 +6,14 @@ import express from "express";
 import "@/shared/infra/di/container";
 import "@/shared/infra/database/TypeormClient";
 
+import { userRoutes } from "./modules/users/presentation/http/routes/userRoutes";
+
 const app = express();
 
 app.use(express.json());
 
 const PORT = envConfig.getPort();
+
+app.use("/users", userRoutes);
 
 app.listen(PORT, () => console.log(`App rodando na porta: ${PORT}`));
