3c9f740618d22da656fe867935a6a53a9c35c37e
AtlasM30 <moreirarthur@gmail.com>
2025-08-18 18:41:50 -0300

    Uptade of movie constainer dependency injection

--- src/modules/movies/infra/di/container.ts
+++ src/modules/movies/infra/di/container.ts
@@ -1,5 +1,20 @@
 import { container } from "tsyringe";
 import { IMoviesRepository } from "../../domain/repositories/IMoviesRepository";
 import { MovieRepository } from "../repositories/MovieRepository";
+import { IMovieUniquenessCheckerService } from "../../domain/services/contracts/IMovieUniquenessCheckerService";
+import { MovieUniquenessCheckerService } from "../../domain/services/MovieUniquenessCheckerService";
+import { IMovieWatchedService } from "../../domain/services/contracts/IMovieWatchedService";
+import { MovieWatchedService } from "../../domain/services/MovieWatchedService";
 
-container.register<IMoviesRepository>("MovieRepository", MovieRepository);
+container.register<IMoviesRepository>("MovieRepository", {
+  useClass: MovieRepository,
+});
+container.register<IMovieUniquenessCheckerService>(
+  "MovieUniquenessCheckerService",
+  {
+    useClass: MovieUniquenessCheckerService,
+  },
+);
+container.register<IMovieWatchedService>("MovieWatchedService", {
+  useClass: MovieWatchedService,
+});
