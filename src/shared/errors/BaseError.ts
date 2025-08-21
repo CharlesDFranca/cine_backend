5f2fc17e8db7e93297cfbdceef11bee40746d109
CharlesDFranca <josecharlestill@gmail.com>
2025-08-20 23:11:25 -0300

    add 'context' in BaseError details

--- src/shared/errors/BaseError.ts
+++ src/shared/errors/BaseError.ts
@@ -2,10 +2,13 @@ export abstract class BaseError extends Error {
   abstract code: string;
   details? = {};
 
-  constructor(message: string, details?: unknown) {
+  constructor(message: string, details?: Record<string, unknown>) {
     super(message);
     this.name = new.target.name;
-    this.details = details || {};
+    this.details = {
+      ...details,
+      context: new.target.name,
+    };
     Error.captureStackTrace(this, this.constructor);
   }
 }
